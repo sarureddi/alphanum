6d821739cc03bee567e81a44f1957bf3ca4fac30
saru <sarumathiayyappan@gmail.com>
2019-06-06 11:49:40 +0530

    Create alphanum.py

---
+++ alphanum.py
@@ -0,0 +1,7 @@
+string=input("enter the value")
+if( string. isdigit()):
+    print("given string contains only number")
+elif(string. isdigit()):
+    print("given string contains only string")
+else:
+    print("given string contains both number & string ")
